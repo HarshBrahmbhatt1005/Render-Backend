7bba82981c951d397026df682c0d3a8ecc309c66
Harsh Brahmbhatt <139995478+HarshBrahmbhatt1005@users.noreply.github.com>
2025-10-09 18:23:33 +0530

    Updated backend files

--- models/Application.js
+++ models/Application.js
@@ -1,30 +1,35 @@
 import mongoose from "mongoose";
 
-const applicationSchema = new mongoose.Schema({
-  code: String,
-  name: String,
-  mobile: String,
-  email: String,
-  product: String,
-  amount: String,
-  bank: String,
-  bankerName: String,
-  status: String,
-  loginDate: String,
-  sales: String,
-  ref: String,
-  sourceChannel: String,
-  remark: { type: String, required: true }, // 🔹 new
-  approvalStatus: { type: String, default: "" },
-  payout: String,
-  expenceAmount: String,
-  feesRefundAmount: String,
-  propertyDetails: String,
-  mktValue: String,
-  roi: String,
-  processingFees: String,
-  auditData: String,
-  consulting: String,
-});
-
+const applicationSchema = new mongoose.Schema(
+  {
+    code: String,
+    otherCode: String,
+    name: String,
+    mobile: String,
+    email: String,
+    product: String,
+    otherProduct: String,
+    amount: String,
+    bank: String,
+    otherBank: String,
+    bankerName: String,
+    status: String,
+    loginDate: String,
+    sales: String,
+    ref: String,
+    sourceChannel: String,
+    remark: String,
+    approvalStatus: String,
+    payout: String,
+    expenceAmount: String,
+    feesRefundAmount: String,
+    propertyDetails: String,
+    mktValue: String,
+    roi: String,
+    processingFees: String,
+    auditData: String,
+    consulting: String,
+  },
+  { timestamps: true }
+);
 export default mongoose.model("Application", applicationSchema);
