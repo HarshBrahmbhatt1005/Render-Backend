29750d64f6206ea6506a4c3cc07b7394981aef3c
Harsh Brahmbhatt <139995478+HarshBrahmbhatt1005@users.noreply.github.com>
2025-11-04 12:52:11 +0530

    Updated Backend

--- models/BuilderVisitData.js
+++ models/BuilderVisitData.js
@@ -31,7 +31,6 @@ const builderVisitSchema = new mongoose.Schema(
     expectedCompletionDate: Date,
     financingRequirements: String,
     residentType: String,
-    // marketValue: String,
     avgAgreementValue: Number,
     nearbyProjects: String,
     surroundingCommunity: String,
