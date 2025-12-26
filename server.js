8a30d15e1e80142dea755d197b748348582932bd
Harsh Brahmbhatt <139995478+HarshBrahmbhatt1005@users.noreply.github.com>
2025-12-26 17:12:18 +0530

    kk

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+
 import path from "path";
 import { fileURLToPath } from "url";
 import Application from "./models/Application.js";
